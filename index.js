cb2bdc47df0f9328db17b1ce6fae5e72cfac4324
Chris <chrisharwell72@gmail.com>
2020-07-17 17:52:38 -0400

    Need to add error boundary for bio and name when sending a put request
    Need to add a delete request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const shortid = require("shortid");
 const server = express();
 server.use(express.json());
 
-const PORT = 8000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Listening on localhost: ${PORT}`);
 });
@@ -13,42 +13,79 @@ let users = [];
 
 // GET
 server.get("/api/users", (req, res) => {
-  res.json(users);
+  try {
+    res.json(users);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ errorMessage: "The users information could not be retrieved." });
+  }
 });
 
 server.get("/api/users/:id", (req, res) => {
-  const { id } = req.params();
-});
+  const { id, name, bio } = req.params;
+  const changes = req.body;
 
+  changes.id = id;
 
-// POST 
-server.post("/api/users", (req, res) => {
-    const userData = req.body;
-    userData.id = shortid.generate();
+  try {
+    let index = users.findIndex((user) => user.id === id);
 
-    users.push(userData);
-
-    res.status(201).json(userData);  
-});
-
-// PUT 
-server.put("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
-    changes.id = id;
-    try {
-      let index = users.findIndex((user) => user.id === id);
-  
-      if (index !== -1) {
-        users[index] = changes;
-        res.status(200).json(users[index]);
-      } else {
-        res.status(404).json({ message: "user id not found" });
-      }
-    } catch (err) {
-      res.status(500).json({ message: "error: ", err });
+    if (index !== -1) {
+      users[index] = changes;
+      res.status(200).json(users[index]);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
     }
-  });
+  } catch (err) {
+    res.status(500).json({ message: "error: ", err });
+  }
+});
 
+// POST
+server.post("/api/users", (req, res) => {
+  const userData = req.body;
+  userData.id = shortid.generate();
 
+  try {
+    if (userData.name && userData.bio) {
+      users.push(userData);
+      res.status(201).json(userData);
+    } else {
+      res.status(404).json("Bad Request");
+    }
+  } catch (error) {
+    res.status(500).json({ message: "error: ", err });
+  }
+});
 
+// PUT
+server.put("/api/users/:id", (req, res) => {
+  const { id, name, bio } = req.params;
+  const changes = req.body;
+
+  changes.id = id;
+  changes.name = name;
+  changes.bio = bio;
+
+  // if (!name || !bio) {
+  //   console.log(name);
+  //   res
+  //     .status(400)
+  //     .json({ errorMessage: "Please provide name and bio for the user." });
+  // }
+
+  try {
+    let index = users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      users[index] = changes;
+      res.status(200).json(users[index]);
+    } else {
+      res.status(404).json({ message: "user id not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error: ", err });
+  }
+});
